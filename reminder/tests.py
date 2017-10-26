9ced0fa17062b754a28d61f6d1f4f4f06527a28a
Rub4ek <rub4ek@gmail.com>
2017-10-26 03:48:09 +0300

    Add tests.

--- reminder/tests.py
+++ reminder/tests.py
@@ -2,5 +2,46 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from reminder.models import Reminder
+
+
+class ReminderTests(APITestCase):
+
+    def test_create_reminder(self):
+        url = reverse('reminder-list')
+        response = self.client.post(url, {'email': 'test@example.com', 'delay': 1, 'text': 'Test'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Reminder.objects.count(), 1)
+        self.assertEqual(Reminder.objects.get().email, 'test@example.com')
+
+    def test_update_reminder(self):
+        reminder = Reminder.objects.create(email='test@example.com', delay=1)
+        url = reverse('reminder-detail', kwargs={'pk': reminder.pk})
+        response = self.client.patch(url, {'email': 'updated@example.com'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Reminder.objects.count(), 1)
+        self.assertEqual(Reminder.objects.get().email, 'updated@example.com')
+
+    def test_delete_reminder(self):
+        reminder = Reminder.objects.create(email='test@example.com', delay=1)
+        url = reverse('reminder-detail', kwargs={'pk': reminder.pk})
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_reminder(self):
+        Reminder.objects.create(email='test@example.com', delay=1)
+        url = reverse('reminder-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_detail_reminder(self):
+        reminder = Reminder.objects.create(email='test@example.com', delay=1)
+        url = reverse('reminder-detail', kwargs={'pk': reminder.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Reminder.objects.get().email, 'test@example.com')
--- todo/tests.py
+++ todo/tests.py
@@ -2,5 +2,89 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from todo.models import Board, Todo
+
+
+class BoardsTests(APITestCase):
+
+    def test_create_board(self):
+        url = reverse('board-list')
+        response = self.client.post(url, {'name': 'Test'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Board.objects.count(), 1)
+        self.assertEqual(Board.objects.get().name, 'Test')
+
+    def test_update_board(self):
+        board = Board.objects.create(name='Test')
+        url = reverse('board-detail', kwargs={'pk': board.pk})
+        response = self.client.patch(url, {'name': 'Updated'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Board.objects.count(), 1)
+        self.assertEqual(Board.objects.get().name, 'Updated')
+
+    def test_delete_board(self):
+        board = Board.objects.create(name='Test')
+        url = reverse('board-detail', kwargs={'pk': board.pk})
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_board(self):
+        Board.objects.create(name='Test')
+        url = reverse('board-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_detail_board(self):
+        board = Board.objects.create(name='Test')
+        url = reverse('board-detail', kwargs={'pk': board.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Board.objects.get().name, 'Test')
+
+
+class TodosTests(APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TodosTests, cls).setUpClass()
+        cls.board = Board.objects.create(name='Test')
+
+    def test_create_todo(self):
+        url = reverse('todo-list', kwargs={'board_pk': self.board.pk})
+        response = self.client.post(url, {'title': 'Test', 'done': True}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertEqual(Todo.objects.get().title, 'Test')
+
+    def test_update_todo(self):
+        todo = Todo.objects.create(title='Test', board_id=self.board.id)
+        url = reverse('todo-detail', kwargs={'board_pk': self.board.pk, 'pk': todo.pk})
+        response = self.client.patch(url, {'title': 'Updated'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertEqual(Todo.objects.get().title, 'Updated')
+
+    def test_delete_todo(self):
+        todo = Todo.objects.create(title='Test', board_id=self.board.id)
+        url = reverse('todo-detail', kwargs={'board_pk': self.board.pk, 'pk': todo.pk})
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_todo(self):
+        Todo.objects.create(title='Test', board_id=self.board.id)
+        url = reverse('todo-list', kwargs={'board_pk': self.board.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_detail_todo(self):
+        todo = Todo.objects.create(title='Test', board_id=self.board.id)
+        url = reverse('todo-detail', kwargs={'board_pk': self.board.pk, 'pk': todo.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.get().title, 'Test')
